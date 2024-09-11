26d0c904cf12876345e4bc62e521689d12759519
Carminelm <carminelm54@gmail.com>
2024-09-11 15:41:16 +0200

    add model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,10 @@
 
 @section('content')
 <div class="container my-5">
-    <h1>{{ $title }}</h1>
+    <h1>Home </h1>
     <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
     <p>
-        {{ $text }}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur totam magni consectetur voluptatem earum, cumque sint dolores non culpa, doloremque nostrum obcaecati iste repellat natus molestias, aperiam ullam eveniet odit.
     </p>
 </div>
 
